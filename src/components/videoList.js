c85fd2f427582186fe196fe15c22a31cc1e4d2bd
letItBezhumphries <ezhumphries@gmail.com>
2018-08-17 18:47:27 -0700

    ExampleVideData loaded

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,4 +1,13 @@
 angular.module('video-player')
 .component('videoList', {
-  // TODO
+  bindings: {
+    videos: '<'
+  },
+
+  templateUrl: 'src/templates/videoList.html',
+  
+  controller: function()  {
+
+  },
+
 });
