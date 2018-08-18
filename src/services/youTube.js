f3ccac241cf6f63110749d6721d595af51a9b7fa
Anthony <Anthony@computers-MacBook-Pro.local>
2018-08-18 00:29:01 -0700

    tarted adding in youtube API

--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,20 +1,20 @@
-angular.module('video-player')
-  .service('youTube', function() {
-     this.search = function(query, cb) {
+// angular.module('video-player')
+//   .service('youTube', function() {
+//      this.search = function(query, cb) {
 
-      $http.get('https://www.googleapis.com/youtube/v3/search', {
-        params: {
-          part: 'snippet',
-          q: 'query',
-          type: 'video',
-          maxResults: 10,
-          key: $window.YOUTUBE_API_KEY,
-          videoEmbeddable: 'true',
-        }
-      })
-      .then(function({data}) {
-        if (cb) { cb(data.items); 
-        console.log('logged'); }
-      });
-     };
-  });
+//       $http.get('https://www.googleapis.com/youtube/v3/search', {
+//         params: {
+//           part: 'snippet',
+//           q: 'query',
+//           type: 'video',
+//           maxResults: 10,
+//           key: $window.YOUTUBE_API_KEY,
+//           videoEmbeddable: 'true',
+//         }
+//       })
+//       .then(function({data}) {
+//         if (cb) { cb(data.items); 
+//         console.log('logged'); }
+//       });
+//      };
+//   });
